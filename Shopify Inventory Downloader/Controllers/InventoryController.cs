55a3709831821a2d92ea7451d152b761812662ce
eldho <eldhose1@outlook.com>
2018-07-24 14:46:20 +0530

    Database handler added to the

--- Shopify Inventory Downloader/Controllers/InventoryController.cs
+++ Shopify Inventory Downloader/Controllers/InventoryController.cs
@@ -3,7 +3,10 @@ using RestSharp;
 using Shopify_Inventory_Downloader.Tools;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -28,42 +31,92 @@ namespace Shopify_Inventory_Downloader.Controllers
 
         public ActionResult auth(string shop, string code)
         {
-            string u = string.Format("https://{0}/admin/oauth/access_token", shop);
+            //First, we need to verify if this is freom shopify
+            if (IsAuthenticRequest(Request.QueryString,secretKey))
+            {
+                string u = string.Format("https://{0}/admin/oauth/access_token", shop);
 
-            var client = new RestClient(u);
+                var client = new RestClient(u);
 
-            var request = new RestRequest(Method.POST);
+                var request = new RestRequest(Method.POST);
 
-            request.RequestFormat = DataFormat.Json;
-            request.AddHeader("Content-Type", "application/json");
+                request.RequestFormat = DataFormat.Json;
+                request.AddHeader("Content-Type", "application/json");
 
-            request.AddParameter("application/x-www-form-urlencoded", "client_id=" + apiKey + "&client_secret=" + secretKey + "&code=" + code, ParameterType.RequestBody);
+                request.AddParameter("application/x-www-form-urlencoded", "client_id=" + apiKey + "&client_secret=" + secretKey + "&code=" + code, ParameterType.RequestBody);
 
-            var response = client.Execute(request);
+                var response = client.Execute(request);
 
-            var r =JsonConvert.DeserializeObject<dynamic>(response.Content);
-            var access = r.access_token;
-            /*
+                var r = JsonConvert.DeserializeObject<dynamic>(response.Content);
+                var access = r.access_token;
 
 
-            //Part 5
-            //create a un-install web hook
-            //you want to know when customers delete your app from their shop
+                DatabaseHandler.SaveShopToken((String)access);
 
-            string unInstallUrlCallback = "https://549653d4.ngrok.io/fulfillment/uninstall";
+                /*
 
-            string shopAdmin = string.Format("https://{0}/admin/", shop);
 
-            var webHook = new WebHookBucket();
-            webHook.Whook = new WebHook { Format = "json", Topic = "app/uninstalled", Address = unInstallUrlCallback };
+                //Part 5
+                //create a un-install web hook
+                //you want to know when customers delete your app from their shop
 
-            CreateUninstallHook(shopAdmin, "webhooks.json", Method.POST, (string)accessToken, webHook);
+                string unInstallUrlCallback = "https://549653d4.ngrok.io/fulfillment/uninstall";
 
-        */
+                string shopAdmin = string.Format("https://{0}/admin/", shop);
+
+                var webHook = new WebHookBucket();
+                webHook.Whook = new WebHook { Format = "json", Topic = "app/uninstalled", Address = unInstallUrlCallback };
+
+                CreateUninstallHook(shopAdmin, "webhooks.json", Method.POST, (string)accessToken, webHook);
+
+            */
+
+
+            }
 
             return View();
         }
 
+
+
+
+        public static bool IsAuthenticRequest(NameValueCollection querystring, string shopifySecretKey)
+        {
+            string hmac = querystring.Get("hmac");
+
+            if (string.IsNullOrEmpty(hmac))
+            {
+                return false;
+            }
+
+            Func<string, bool, string> replaceChars = (string s, bool isKey) =>
+            {
+                //Important: Replace % before replacing &. Else second replace will replace those %25s.
+                string output = (s?.Replace("%", "%25").Replace("&", "%26")) ?? "";
+
+                if (isKey)
+                {
+                    output = output.Replace("=", "%3D");
+                }
+
+                return output;
+            };
+
+            var kvps = querystring.Cast<string>()
+                .Select(s => new { Key = replaceChars(s, true), Value = replaceChars(querystring[s], false) })
+                .Where(kvp => kvp.Key != "signature" && kvp.Key != "hmac")
+                .OrderBy(kvp => kvp.Key)
+                .Select(kvp => $"{kvp.Key}={kvp.Value}");
+
+            var hmacHasher = new HMACSHA256(Encoding.UTF8.GetBytes(shopifySecretKey));
+            var hash = hmacHasher.ComputeHash(Encoding.UTF8.GetBytes(string.Join("&", kvps)));
+
+            //Convert bytes back to string, replacing dashes, to get the final signature.
+            var calculatedSignature = BitConverter.ToString(hash).Replace("-", "");
+
+            //Request is valid if the calculated signature matches the signature from the querystring.
+            return calculatedSignature.ToUpper() == hmac.ToUpper();
+        }
 
 
     }
---
+++ Shopify Inventory Downloader/Tools/DatabaseHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Shopify_Inventory_Downloader.Tools
+{
+    public class DatabaseHandler
+    {
+        //global variables
+
+
+        //Connection variables
+        static SqlConnection con = new SqlConnection(" Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename=\"|DataDirectory|\\Database.mdf\";Integrated Security = True"  );
+        static SqlCommand cmd;
+        static SqlDataReader dr;
+
+        
+        //functions
+
+        //Save a ShopName into the database
+        public static void SaveShopToken(String token)
+        {
+            String  SQL_Syntax = "INSERT INTO SystemTable (Value)   VALUES( @ShopToken ) WHERE Name= 'AccessToken' ";
+            con.Open();
+            cmd = new SqlCommand(SQL_Syntax);
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("@ShopToken", token);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            Console.WriteLine("DEBUG    :   token is inserted now please manually check the database to ensure correctness: ");
+
+            return;
+        }
+
+        private void SaveShopName(String ShopName)
+        {
+            String SQL_Syntax = "INSERT INTO SystemTable (Value)   VALUES( @ShopName ) WHERE Name= 'ShopName' ";
+            con.Open();
+            cmd = new SqlCommand(SQL_Syntax);
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("@ShopName", ShopName);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            Console.WriteLine("DEBUG    :   ShopName is inserted now please manually check the database to ensure correctness: ");
+
+            return;
+        }
+
+        //get the shopToken from Database
+        private String getShopToken()
+        {
+            String result, SQL_Syntax = "SELECT Value FROM SystemTable WHERE Name= 'AccessToken' ";
+            cmd = new SqlCommand(SQL_Syntax, con);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            result = dr[0].ToString();
+            con.Close();
+            
+            //Outputs
+            Console.WriteLine("DEBUG    :   access token fetched from the database in the getShopToken() is : " + result);
+            return result;
+
+        }
+        
+        //Next function description goes here
+    }
+}
